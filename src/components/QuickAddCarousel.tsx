f9abf4582c1b1046b6d2053ad09de182b6bd2bff
CJ Karkut <cjkarkut@gmail.com>
2024-12-07 14:11:18 -0600

    style quickaddcarousel

--- src/components/QuickAddCarousel.tsx
+++ src/components/QuickAddCarousel.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Slider from "react-slick";
-import { Button, Box } from "@mui/material";
+import { Button, Box, useMediaQuery, useTheme } from "@mui/material";
 
 const QuickAddCarousel: React.FC = () => {
+	const theme = useTheme();
+	const isMdScreen = useMediaQuery(theme.breakpoints.up("sm"));
+	const isLgScreen = useMediaQuery(theme.breakpoints.up("md"));
+
+	const { slidesToShow, slidesToScroll } = useMemo(() => {
+		if (isLgScreen) {
+			return { slidesToShow: 7, slidesToScroll: 6 };
+		} else if (isMdScreen) {
+			return { slidesToShow: 5, slidesToScroll: 4 };
+		} else {
+			return { slidesToShow: 3, slidesToScroll: 1 };
+		}
+	}, [isLgScreen, isMdScreen]);
+
+	const settings = {
+		dots: false,
+		infinite: true,
+		speed: 1000,
+		slidesToShow,
+		slidesToScroll,
+	};
+
 	const profiles = [
 		{ id: 1, image: "https://picsum.photos/200", name: "John Doe" },
 		{ id: 2, image: "https://picsum.photos/200", name: "Jane Smith" },
@@ -12,30 +34,8 @@ const QuickAddCarousel: React.FC = () => {
 		{ id: 3, image: "https://picsum.photos/200", name: "Emily Brown" },
 	];
 
-	const settings = {
-		dots: false,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 2,
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 5,
-				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-		],
-	};
-
 	return (
-		<Box sx={{ width: "90%", margin: ".5rem auto" }}>
+		<Box sx={{ width: "90%", margin: "0 auto" }}>
 			<Slider {...settings}>
 				{profiles.map((profile) => (
 					<Box
